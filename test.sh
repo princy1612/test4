d10188ba07c2075c50218bc5e078ce8f2b10115d
princy1612 <princymarina1612@gmail.com>
2020-08-31 19:33:48 +0530

    test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+echo "start testing the shell"
+ for i in {1..5}
+ do 
+  echo "hello world"
+   done
+    echo "test version of node,npm,java,git"
+	#node -v;
+	#npm -v;
+	#git --version;
+	#java -version;
+	 echo "end the testing"
+	 #cat test.sh
